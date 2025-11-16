94e2e6a7741cba6a0ec1817cafd3401e6d231c2d
Leona Redmann <leored@gmx.de>
2025-11-16 23:07:33 +0100

    theme switcher optimzed

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -2,9 +2,9 @@ export default function SiteFooter() {
     return (
     <footer className="footer">
     <div className="footerContact">
-        <p>Email: <a href="mailto:leona.josephine@example.com">leona.josephine@example.com</a></p>
+        <p>Email: <a href="mailto:leona.redmann@gmx.net">leona.redmann@gmx.net</a></p>
         <p>|</p>
-        <p>Phone: <a href="tel:+1234567890">+1 (234) 567-890</a></p>
+        <p>Phone: <a href="tel:+491731583246">+49 173 158 32 46</a></p>
     </div>
     <p>© 2025 Leona Josephine. All rights reserved.</p>
     </footer>
